00f23a256d4137def9a035a8da6a4f6707ce492a
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-02-04 15:30:16 +0200

    done task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients.forEach((ingredient) => {
+  const list = document.createElement("li");
+  list.classList.add("item");
+  ingredientsRef.append(list);
+  list.textContent = ingredient;
+});
+
+console.log(ingredientsRef);
