cdde0c3ba5293b8d24130779088ea4f48b386287
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-02-05 18:04:19 +0200

    done task 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputRef = document.querySelector('#validation-input');
+
+const checkSymbols = () => {
+    if (inputRef.value.length === Number(inputRef.dataset.length)) {
+        inputRef.classList.remove(`invalid`);
+        inputRef.classList.add(`valid`);
+        return;
+    } else {
+        inputRef.classList.remove(`valid`);
+        inputRef.classList.add(`invalid`);
+    }
+};
+
+inputRef.addEventListener('blur', checkSymbols);
