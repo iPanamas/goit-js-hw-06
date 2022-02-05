8d7febb8f1b7a011e80862f46808a000bb3c2750
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-02-05 20:00:26 +0200

    done 7-8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены!');
+    }
+
+    const objectForm = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(objectForm);
+    event.currentTarget.reset();
+}
