0e6cf63c4915af36102a4dfaa271a0029daf324d
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-02-06 14:42:48 +0200

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const buttonRef = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+const changeBodyColor = () => {
+    document.body.style.background = getRandomHexColor();
+    color.innerHTML = getRandomHexColor();
+};
+
+buttonRef.addEventListener('click', changeBodyColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
